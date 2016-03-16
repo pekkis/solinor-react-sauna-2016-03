846e8d86625eebe08edab8f0bbfb5bcae363df2f
Mikko Forsström <puhemies@diktaattoriporssi.com>
2016-03-16 09:54:23 +0200

    Removed everything again.

--- src/client.js
+++ src/client.js
@@ -1,22 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { browserHistory } from 'react-router';
-import { createStore } from '@dr-kobros/react-broilerplate/lib/redux';
-import { createApp } from '@dr-kobros/react-broilerplate/lib/app';
-import * as reducers from './reducers';
-import { createRouter } from './router';
 
-const { store, history } = createStore(reducers, browserHistory);
-
-const router = createRouter({
-    store,
-    history
-});
-
-const app = createApp(store, history, router);
-
-ReactDOM.render(
-    app,
-    document.getElementById('app')
-);
-
+console.log('All your base are belong to us');
